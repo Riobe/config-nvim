69a358156930711d0f9e00519e60c5504ca1a3df
Jeremy Pridemore <jeremy.pridemore@allegion.com>
2022-05-05 17:56:12 -0600

    Remove echos from lspconfig.lua

--- plugin/nvim-lspconfig.lua
+++ plugin/nvim-lspconfig.lua
@@ -3,7 +3,6 @@
 local on_attach = function(client, bufnr)
   -- Enable completion triggered by <c-x><c-o>
   vim.api.nvim_buf_set_option(bufnr, 'omnifunc', 'v:lua.vim.lsp.omnifunc')
-  vim.cmd('echomsg "lspconfig on_attach"')
 end
 
 -- Python     -> Pyright
@@ -11,7 +10,6 @@ end
 -- YAML -> yamlls
 local servers = { 'pyright', 'tsserver', 'yamlls' }
 for _, lsp in pairs(servers) do
-  vim.cmd('echomsg "Setting up server for server: ' .. lsp .. '"')
   require('lspconfig')[lsp].setup{
     on_attach = on_attach,
     flags = {
