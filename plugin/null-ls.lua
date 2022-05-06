3a43906071915b5d02550c668602e460b34c244e
Jeremy Pridemore <jeremy.pridemore@allegion.com>
2022-05-05 18:47:44 -0600

    Fix to handle lsp_formatting in neovim 0.7.0.

--- plugin/null-ls.lua
+++ plugin/null-ls.lua
@@ -1,13 +1,17 @@
-local lsp_formatting = function(bufnr)
-    vim.lsp.buf.format({
-        filter = function(clients)
-            -- filter out clients that you don't want to use
-            return vim.tbl_filter(function(client)
-                return client.name ~= "tsserver"
-            end, clients)
-        end,
-        bufnr = bufnr,
-    })
+local util = require('vim.lsp.util')
+
+local lsp_formatting = function(client, bufnr)
+  local params = util.make_formatting_params({})
+  client.request('textDocument/formatting', params, nil, bufnr);
+  -- vim.lsp.buf.format({
+  --     filter = function(clients)
+  --         -- filter out clients that you don't want to use
+  --         return vim.tbl_filter(function(client)
+  --             return client.name ~= "tsserver"
+  --         end, clients)
+  --     end,
+  --     bufnr = bufnr,
+  -- })
 end
 
 -- if you want to set up formatting on save, you can use this as a callback
@@ -15,16 +19,16 @@ local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
 
 -- add to your shared on_attach callback
 local on_attach = function(client, bufnr)
-    if client.supports_method("textDocument/formatting") then
-        vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
-        vim.api.nvim_create_autocmd("BufWritePre", {
-            group = augroup,
-            buffer = bufnr,
-            callback = function()
-                lsp_formatting(bufnr)
-            end,
-        })
-    end
+  if client.supports_method("textDocument/formatting") then
+    vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
+    vim.api.nvim_create_autocmd("BufWritePre", {
+      group = augroup,
+      buffer = bufnr,
+      callback = function()
+        lsp_formatting(client, bufnr)
+      end,
+    })
+  end
 end
 
 require('null-ls').setup({
