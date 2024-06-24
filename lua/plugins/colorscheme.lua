a7070d4d6046684fa1e2a3c83e4c47a1e60eb2b9
Riobe <jeremy.pridemore@gmail.com>
2024-06-24 16:36:13 -0600

    Add the catppuccin-mocha color scheme

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,9 +1,11 @@
 return {
-  -- Configure LazyVim to load evening
+  -- Configure LazyVim to load my color scheme
+  { "catppuccin/nvim", name = "catppuccin", priority = 1000 },
   {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = { "evening" },
+      colorscheme = { "catppuccin-mocha" },
+      -- colorscheme = { "evening" },
       -- use('tomasiser/vim-code-dark')
     },
   },
