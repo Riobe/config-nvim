1aeee4e122d416a7ff3e3729c567e2de51a4e39d
Riobe <jeremy.pridemore@gmail.com>
2024-07-15 14:20:45 -0600

    Temporarily remove custom nvim-cmp mappings while working to fix.

--- lua/plugins/nvim-cmp.lua
+++ lua/plugins/nvim-cmp.lua
@@ -15,33 +15,35 @@ return {
       { name = "luasnip" },
     })
 
-    opts.mapping = vim.tbl_extend("force", opts.mapping, {
-      ["<Tab>"] = cmp.mapping.confirm({ select = true }),
-
-      ["<C-d>"] = cmp.mapping(function(fallback)
-        if cmp.visible() then
-          -- You could replace select_next_item() with confirm({ select = true }) to get VS Code autocompletion behavior
-          cmp.select_next_item()
-        elseif vim.snippet.active({ direction = 1 }) then
-          vim.schedule(function()
-            vim.snippet.jump(1)
-          end)
-        else
-          fallback()
-        end
-      end, { "i", "s" }),
-
-      ["<C-u>"] = cmp.mapping(function(fallback)
-        if cmp.visible() then
-          cmp.select_prev_item()
-        elseif vim.snippet.active({ direction = -1 }) then
-          vim.schedule(function()
-            vim.snippet.jump(-1)
-          end)
-        else
-          fallback()
-        end
-      end, { "i", "s" }),
-    })
+    -- opts.mapping = vim.tbl_extend("force", opts.mapping, {
+    --   ["<Tab>"] = cmp.mapping.confirm({ select = true }),
+    --   ["<C-d>"] = cmp.mapping.scroll_docs(1),
+    --   ["<C-u>"] = cmp.mapping.scroll_docs(-1),
+    --
+    --   -- ["<C-d>"] = cmp.mapping(function(fallback)
+    --   --   if cmp.visible() then
+    --   --     -- You could replace select_next_item() with confirm({ select = true }) to get VS Code autocompletion behavior
+    --   --     cmp.select_next_item()
+    --   --   elseif vim.snippet.active({ direction = 1 }) then
+    --   --     vim.schedule(function()
+    --   --       vim.snippet.jump(1)
+    --   --     end)
+    --   --   else
+    --   --     fallback()
+    --   --   end
+    --   -- end, { "i", "s" }),
+    --   --
+    --   -- ["<C-u>"] = cmp.mapping(function(fallback)
+    --   --   if cmp.visible() then
+    --   --     cmp.select_prev_item()
+    --   --   elseif vim.snippet.active({ direction = -1 }) then
+    --   --     vim.schedule(function()
+    --   --       vim.snippet.jump(-1)
+    --   --     end)
+    --   --   else
+    --   --     fallback()
+    --   --   end
+    --   -- end, { "i", "s" }),
+    -- })
   end,
 }
