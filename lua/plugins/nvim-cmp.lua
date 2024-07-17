b37bbab13b2535a9cf4adf16c9d39cf95049c52a
Riobe <jeremy.pridemore@gmail.com>
2024-07-17 15:36:12 -0600

    Get nvim-cmp down to minimum for what I want.

--- lua/plugins/nvim-cmp.lua
+++ lua/plugins/nvim-cmp.lua
@@ -5,13 +5,11 @@ return {
   opts = function(_, opts)
     local cmp = require("cmp")
 
-    opts.sources = vim.tbl_extend("force", opts.sources, {
-      { name = "luasnip" },
-    })
-
     opts.mapping = vim.tbl_extend("force", opts.mapping, {
       ["<CR>"] = cmp.config.disable,
+      ["<C-u>"] = cmp.mapping.select_prev_item(),
       ["<C-p>"] = cmp.mapping.select_prev_item(),
+      ["<C-d>"] = cmp.mapping.select_next_item(),
       ["<C-n>"] = cmp.mapping.select_next_item(),
       ["<C-e>"] = cmp.mapping.abort(),
       ["<C-f>"] = cmp.mapping.confirm({ select = true }),
