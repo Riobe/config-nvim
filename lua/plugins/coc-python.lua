94a30c7f29d2d02db72955a163a9a26f3d2f6a7c
Riobe <jeremy.pridemore@gmail.com>
2026-05-21 19:29:12 -0500

    Updates to handle python linting

---
+++ lua/plugins/coc-python.lua
@@ -0,0 +1,2 @@
+return {}
+-- return { "fannheyward/coc-pyright" }
--- lua/plugins/nvim-lspconfig.lua
+++ lua/plugins/nvim-lspconfig.lua
@@ -9,3 +9,21 @@ return {}
 --     },
 --   },
 -- }
+-- return {
+--   "neovim/nvim-lspconfig",
+--   opts = {
+--     servers = {
+--       ruff = {
+--         -- cmd_env = { RUFF_TRACE = "messages" },
+--         init_options = {
+--           settings = {
+--             logLevel = "debug",
+--           },
+--         },
+--         format = {
+--           ["quote-style"] = "single",
+--         },
+--       },
+--     },
+--   },
+-- }
