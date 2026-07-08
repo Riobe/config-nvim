82a9dc9e5fc9fdffa6680f94c3c38487759483f1
Jeremy Pridemore <jeremy.pridemore@1800gotjunk.com>
2026-07-08 08:00:34 -0500

    Customize the dashboard when opening vim

---
+++ lua/plugins/snacks.lua
@@ -0,0 +1,45 @@
+return {
+  "folke/snacks.nvim",
+  ---@type snacks.Config
+  opts = {
+    dashboard = {
+      ---@class snacks.dashboard.Config
+      ---@field enabled? boolean
+      ---@field sections snacks.dashboard.Section
+      ---@field formats table<string, snacks.dashboard.Text|fun(item:snacks.dashboard.Item, ctx:snacks.dashboard.Format.ctx):snacks.dashboard.Text>
+      preset = {
+        keys = {
+          { icon = " ", key = "n", desc = "New File", action = ":ene | startinsert" },
+          { icon = " ", key = "p", desc = "Find File", action = ":lua Snacks.dashboard.pick('files')" },
+          { icon = " ", key = "s", desc = "Search Text", action = ":lua Snacks.dashboard.pick('live_grep')" },
+          { icon = " ", key = "P", desc = "Projects", action = ":Telescope projects" },
+          { icon = " ", key = "o", desc = "Old Files", action = ":lua Snacks.dashboard.pick('oldfiles')" },
+          {
+            icon = " ",
+            key = "c",
+            desc = "Config",
+            action = ":lua Snacks.dashboard.pick('files', {cwd = vim.fn.stdpath('config')})",
+          },
+          { icon = " ", key = "r", desc = "Restore Session", section = "session" },
+          { icon = "󰒲 ", key = "l", desc = "Lazy", action = ":Lazy", enabled = package.loaded.lazy ~= nil },
+          {
+            icon = "󰒲 ",
+            key = "x",
+            desc = "Lazy Extras",
+            action = ":LazyExtras",
+            enabled = package.loaded.lazy ~= nil,
+          },
+          { icon = " ", key = "q", desc = "Quit", action = ":qa" },
+        },
+      },
+
+      sections = {
+        { section = "header" },
+        { section = "keys", gap = 1, padding = 1 },
+        { icon = " ", title = "Projects", section = "projects", indent = 2, padding = 1 },
+        { icon = " ", title = "Recent Files", section = "recent_files", indent = 2, padding = 1 },
+        { section = "startup" },
+      },
+    },
+  },
+}
